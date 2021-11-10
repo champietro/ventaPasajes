49d7d96f71067d08ce2dca3539b456a4e5940c9d
Juan Pedro Alcuaz <champietro@users.noreply.github.com>
2021-11-10 20:33:30 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/*
+3) Se necesita vender los pasajes de un colectivo.
+El colectivo tiene 20 filas de 3 butacas cada uno
+Por convención se pone un 0 en el asiento libre 
+y un 1 en el ya vendido. Cuando los pasajes se ponen 
+a la venta están todos los asientos libres 
+Cuando una persona quiere comprar se muestra los 
+lugares desocupados y se le asigna un asiento elegido 
+por el pasajero
+*/
+
+
